ddc847cab8cf0c7d22070fccafa030bae7f408d5
clarababette <clara_coetzee@zoho.com>
2022-02-22 15:56:31 +0200

    scoring added

--- index.js
+++ index.js
@@ -2,9 +2,33 @@ let secretNumber;
 const resultMsg = document.querySelector('.result_msg');
 const guess = document.querySelector('.guess');
 const guessBtn = document.querySelector('.guess_btn');
+const resetBtn = document.querySelector('.reset_btn');
 const newGameMsg = document.querySelector('.new_game_msg');
+let scores = [];
+
+const scoresElem = document.querySelector('.scores');
+let gameScore; 
+
+
+
+function scoreboard() {
+  scoresElem.innerHTML = '<th>Game played on...</th> <th>Score</th>';
+  for (const myScore of scores) {
+    const row = document.createElement('tr');
+    const date = document.createElement('td');
+    const score = document.createElement('td');
+  
+    date.innerHTML = myScore.date;
+    score.innerHTML = myScore.score;
+    row.appendChild(date);
+    row.appendChild(score);
+    scoresElem.appendChild(row)
+  }
+}
 
 function newGame() {
+  scoreboard();
+  gameScore = 100;
   secretNumber = Math.ceil(Math.random() * 100);
   resultMsg.innerHTML = '';
   guess.value = undefined;
@@ -14,11 +38,15 @@ function newGame() {
   }, 3000);
 }
 
+if (localStorage.getItem('scores')) {
+  scores = JSON.parse(localStorage.getItem('scores'));
+  scoreboard()
+}
 newGame();
 
 function checkGuess(guessedNumber) {
-  const tooLow = 'Your guess is too low';
-  const tooHigh = 'Your guess is too high';
+  const tooLow = `${guessedNumber} is too low.`;
+  const tooHigh = `${guessedNumber} is too high.`;
   const correct = `Correct, the secret number is ${secretNumber}`;
   let result =
     guessedNumber == secretNumber
@@ -27,9 +55,28 @@ function checkGuess(guessedNumber) {
       ? tooHigh
       : tooLow;
   resultMsg.innerHTML = result;
-  result == correct ? setTimeout(newGame, 3000) : undefined;
+  const now = new Intl.DateTimeFormat('en-GB', { dateStyle: 'short', timeStyle: 'short' }).format(new Date())
+  if (result == correct) {
+    setTimeout(newGame, 3000);
+    scores.unshift({
+      date: now,
+      score: gameScore
+    })
+    localStorage.setItem('scores', JSON.stringify(scores))
+  } else {
+    gameScore--;
+  }
 }
 
 guessBtn.addEventListener('click', () => {
   checkGuess(guess.value);
 });
+resetBtn.addEventListener('click', () => {
+  localStorage.removeItem('scores');
+});
+
+guess.addEventListener('keyup', (event) => {
+  if (event.keyCode === 13) {
+    guessBtn.click();
+  }
+})
